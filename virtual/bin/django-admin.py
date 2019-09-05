e4c14766c4ad02619b50f55af3d85166e7bf6b0e
nimosaid <saidnimo123@gmail.com>
2019-09-05 09:16:11 +0300

    starting

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nimo/Documents/Moringa-django-projects/Awwards/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
